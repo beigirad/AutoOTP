98f9c8dc218b88ecb35984908d5ef12473906217
Farhad Beigirad <f.beigirad@gmail.com>
2020-07-09 01:40:28 +0430

    added timber and defined application

---
+++ app/src/main/java/ir/beigirad/fishymind/FishyApp.kt
@@ -0,0 +1,15 @@
+package ir.beigirad.fishymind
+
+import android.app.Application
+import timber.log.Timber
+
+/**
+ * Created by Farhad Beigirad on 7/8/20.
+ */
+class FishyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree());
+    }
+}

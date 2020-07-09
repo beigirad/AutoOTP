dd94ad026f4161a27510067a62defa83eb4a7846
Farhad Beigirad <f.beigirad@gmail.com>
2020-07-09 16:23:06 +0430

    improve tests

---
+++ app/src/test/java/ir/beigirad/autootp/OtpCaseFactory.kt
@@ -0,0 +1,35 @@
+package ir.beigirad.autootp
+
+/**
+ * Created by Farhad Beigirad on 7/9/20.
+ */
+object OtpCaseFactory {
+    val saman1 = Case(
+        "رمز دوم پويا\n Code = 756042\nبانک سامان\nاعتبار تا 11:14:46  1399/4/18",
+        "756042"
+    )
+    val saman2 = Case(
+        "رمز پوياي برداشت وجه\n Code = 9381\nبانک سامان\n1399/4/18 \n14:34:39",
+        "9381"
+    )
+
+    val saman3 = Case(
+        "بانك سامان\nواريز مبلغ  30,000,000ريال\nبه  299-100-1112233-1 \nمانده 6,263,200\n1399/4/18\n14:07",
+        null
+    )
+
+    val parsian1 = Case(
+        "*پارسیان بانک ایرانیان*\n  \nرمز یکبار مصرف 57758 \nمهلت استفاده دو دقیقه است.",
+        "57758"
+    )
+
+    val parsian2 = Case(
+        "*پارسیان بانک ایرانیان*\n برداشت از:12345678910\nمبلغ:  1,689,500 ریال \nزمان: 1399/04/14 01:13:24\nمانده: 4,715,348 ریال",
+        null
+    )
+}
+
+data class Case(
+    val message: String,
+    val expectedOtp: String?
+)
--- app/src/test/java/ir/beigirad/autootp/OtpFinderTest.kt
+++ app/src/test/java/ir/beigirad/autootp/OtpFinderTest.kt
@@ -1,5 +1,10 @@
 package ir.beigirad.autootp
 
+import ir.beigirad.autootp.OtpCaseFactory.parsian1
+import ir.beigirad.autootp.OtpCaseFactory.parsian2
+import ir.beigirad.autootp.OtpCaseFactory.saman1
+import ir.beigirad.autootp.OtpCaseFactory.saman2
+import ir.beigirad.autootp.OtpCaseFactory.saman3
 import ir.beigirad.autootp.bankstemplate.ParsianBank
 import ir.beigirad.autootp.bankstemplate.SamanBank
 import org.junit.Assert
@@ -8,25 +13,23 @@ import org.junit.Test
 class OtpFinderTest {
     @Test
     fun saman_otp_detected() {
-        val sms1 = "رمز دوم پويا\n Code = 756042\nبانک سامان\nاعتبار تا 11:14:46  1399/4/18"
-        val sms2 = "رمز پوياي برداشت وجه\n Code = 9381\nبانک سامان\n1399/4/18 \n14:34:39"
-
-        Assert.assertEquals("756042", SamanBank().getCode(sms1))
-        Assert.assertEquals("9381", SamanBank().getCode(sms2))
-
-        val otherSms =
-            "بانك سامان\nواريز مبلغ  30,000,000ريال\nبه  299-100-1112233-1 \nمانده 6,263,200\n1399/4/18\n14:07"
-        Assert.assertNull(SamanBank().getCode(otherSms))
+        Assert.assertEquals(saman1.expectedOtp, SamanBank().getCode(saman1.message))
+        Assert.assertEquals(saman2.expectedOtp, SamanBank().getCode(saman2.message))
+        Assert.assertEquals(saman3.expectedOtp, SamanBank().getCode(saman3.message))
     }
 
     @Test
     fun parsian_otp_detected() {
-        val otpSms =
-            "*پارسیان بانک ایرانیان*\n  \nرمز یکبار مصرف 57758 \nمهلت استفاده دو دقیقه است."
-        Assert.assertEquals("57758", ParsianBank().getCode(otpSms))
+        Assert.assertEquals(parsian1.expectedOtp, ParsianBank().getCode(parsian1.message))
+        Assert.assertEquals(parsian2.expectedOtp, ParsianBank().getCode(parsian2.message))
+    }
 
-        val otherSms =
-            "*پارسیان بانک ایرانیان*\n برداشت از:12345678910\nمبلغ:  1,689,500 ریال \nزمان: 1399/04/14 01:13:24\nمانده: 4,715,348 ریال"
-        Assert.assertNull(ParsianBank().getCode(otherSms))
+    @Test
+    fun otp_finder_works_well() {
+        Assert.assertEquals(saman1.expectedOtp, OtpFinder.find(saman1.message))
+        Assert.assertEquals(saman2.expectedOtp, OtpFinder.find(saman2.message))
+        Assert.assertEquals(saman3.expectedOtp, OtpFinder.find(saman3.message))
+        Assert.assertEquals(parsian1.expectedOtp, OtpFinder.find(parsian1.message))
+        Assert.assertEquals(parsian2.expectedOtp, OtpFinder.find(parsian2.message))
     }
 }
